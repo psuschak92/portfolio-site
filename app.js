aef533925beb47424639eef79af7e02434c55264
psuschak92 <psus92@gmail.com>
2021-02-09 13:23:39 -0500

    Update portfolio-site

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
+// initialize project dependencies using node require 
 const express = require('express');
 const data = require('./data.json');
 const path = require('path');
 const app = express();
 
+// set up middleware including template engine and static files
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
+// set routes to render template views
 app.get('/', (req, res) => {
-    // console.log(data.projects);
     res.locals.projects = data.projects;
     res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    // const err = new Error('Something broke!');
+    // const err = new Error();
     // err.status = 500;
     // throw err;
     res.render('about');
@@ -25,23 +26,24 @@ app.get('/project/:id', (req, res, next) => {
     const id = req.params.id;
     const project = data.projects[id];
 
+    // throw 404 if id out of bounds
     if (!project) {
-        const err = new Error(`Oops, page not found. Looks like the route "${req.url}" does not exist.`);
+        const err = new Error();
         err.status = 404;
         next(err);
     }
     res.render('project', {project});
 });
 
-// 404 error endpoint
+// 404 error handler
 app.use((req, res, next) => {
     console.log('Handling 404 error');
-    const err = new Error(`Oops, page not found.`);
+    const err = new Error();
     err.status = 404;
     next(err);
 });
 
-// Global error endpoint
+// global error handler
 app.use((err, req, res, next) => {
     console.log('Handling a global error');
 
